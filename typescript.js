33597476c769f833fad358eb2b1092179370f62f
Artur Jaakman <jaakmanartur@gmail.com>
2019-07-26 18:50:40 +0300

    Initial Commit

---
+++ typescript.js
@@ -0,0 +1,8 @@
+// tsc typescript.ts --target ES6
+// tsc typescript.ts --watch --target ES6
+{
+    let str = null;
+    str = "Hello World";
+    str = str.toLowerCase();
+    console.log(str);
+}
